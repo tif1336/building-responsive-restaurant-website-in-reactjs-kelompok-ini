bc4497ed77bcbb8e56a9233cb98823fc83a3b93c
Aldin <adnzain40@gmail.com>
2023-12-08 15:40:43 +0700

    add initial page components like navbar

---
+++ restosolo/src/app/page.tsx
@@ -0,0 +1,25 @@
+// export default function Page() {
+//   return(
+//         <h1 className="text-3xl font-bold underline">
+//       Hello world!
+//     </h1>
+
+//   )
+// }
+  
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="container mx-auto px-4">
+        <h1>Hello word</h1>
+        <p>This is a content to make our page longer</p>
+        <div className="w-full h-screen bg-green-300"></div>
+        <p>
+          Lorem Ipsum is simply dummy text ...
+        </p>
+      </div>
+    </>
+  );
+}
