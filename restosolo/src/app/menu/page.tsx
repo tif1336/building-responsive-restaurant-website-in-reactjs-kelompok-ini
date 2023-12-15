a14ce54277d8a007d091c4b82250befbc6b8b3e4
adinghabsyi <adinghabsyi7@gmail.com>
2023-12-15 16:02:40 +0700

    Added title for menu

--- restosolo/src/app/menu/page.tsx
+++ restosolo/src/app/menu/page.tsx
@@ -22,7 +22,6 @@ function truncateText(text: string, sentenceCount: number): string {
   return truncatedText;
 }
 
-
 interface MenuApi {
   name: string;
   image: string;
@@ -59,10 +58,33 @@ export default class MenuContainer extends Component<{}, MenuContainerState> {
     const { menus, activeButton } = this.state;
 
     return (
-      <div className="bg-primary">
-        <Box sx={{ flexGrow: 1 }}>
+      <div id="menu" className="bg-primary">
+        <Typography
+          variant="h5"
+          noWrap
+          component="a"
+          className="text-orange-600 text-lg font-bold"
+          sx={{
+            mr: 2,
+            display: { xs: "flex", md: "flex" },
+            fontFamily: "sans-serif",
+            fontWeight: 700,
+            letterSpacing: ".2rem",
+            color: "inherit",
+            textDecoration: "none",
+            marginBottom: "2rem",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: "4rem",
+          }}
+        >
+          Our Menu
+        </Typography>
+        <Box
+          sx={{ flexGrow: 1, alignItems: "center", justifyContent: "center" }}
+        >
           <Grid
-            className="container items-center justify-center px-4 pb-16"
+            className="container items-center justify-center px-4 pb-16 text-center"
             container
             spacing={2}
             paddingY={2}
@@ -101,11 +123,11 @@ export default class MenuContainer extends Component<{}, MenuContainerState> {
             </Grid>
           </Grid>
           <Grid
-            className="container mx-auto px-10"
+            className="container mx-auto px-10 flex flex-wrap justify-center"
             container
             spacing={2}
             paddingY={2}
-            paddingBottom={6}
+            paddingBottom={18}
           >
             {menus
               .filter(
