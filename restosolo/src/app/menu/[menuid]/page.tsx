55c717f5170ce200e7f705490191094c5d56e494
adinghabsyi <adinghabsyi7@gmail.com>
2023-12-22 10:55:13 +0700

    Modify detail menu

--- restosolo/src/app/menu/[menuid]/page.tsx
+++ restosolo/src/app/menu/[menuid]/page.tsx
@@ -4,58 +4,53 @@ import { Metadata } from "next";
 import React, { Component, useState, useEffect } from "react";
 
 type Props = {
-    params: { menuid: number };
+  params: { menuid: number };
 };
 
-
 interface Menu {
-    id: number;
-    category: string;
-    name: string;
-    image: string;
-    price: number;
-    description: string;
+  id: number;
+  category: string;
+  name: string;
+  image: string;
+  price: number;
+  description: string;
 }
 
 interface MenuProps {
-    menu: Menu
+  menu: Menu;
 }
 
-
 export default function ProductDetails({ params }: Props) {
+  // fetch data from api
+  const [menu, setMenu] = useState<Menu>({} as Menu);
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
-    // fetch data from api
-    const [menu, setMenu] = useState<Menu>({} as Menu);
-    useEffect(() => {
-        fetchMenu();
-    }, []);
+  const url = `https://resto-solo-app-production.up.railway.app/api/menus/${params.menuid}/`;
+  const fetchMenu = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setMenu(data));
+  };
 
-  
-    const url = `https://resto-solo-app-production.up.railway.app/api/menus/${params.menuid}/`;
-    const fetchMenu = () => {
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => setMenu(data));
-    };
-
-    return (
-        <div className="w-full flex bg-primary justify-center align-middle py-80">
-            <div className="w-1/2 flex justify-center align-middle">
-                <div className="w-4/5 h-[480px] my-auto bg-cover rounded-2xl" style={{backgroundImage: `url(${menu.image})`}}>
-                </div>
-            </div>
-            <div className="w-1/2 flex-row my-auto text-white pr-60">
-                <h3 className="text-6xl font-bold ">{menu.name}</h3>
-                <p className="text-4xl font-semibold">${menu.price}</p>
-                <p className="text-2xl text-orange-500 font-semibold border-2 border-r-orange-500 w-fit px-2">{menu.category}</p>
-                <p>Description</p>
-                <p className="text-justify">{menu.description}</p>
-            </div>
-
-
-
-
-        </div>
-    );
-
+  return (
+    <div className="w-full flex bg-primary justify-center align-middle pt-20 pb-24">
+      <div className="w-1/2 flex justify-center align-middle">
+        <div
+          className="w-4/5 h-[480px] my-auto bg-cover rounded-2xl"
+          style={{ backgroundImage: `url(${menu.image})` }}
+        ></div>
+      </div>
+      <div className="w-1/2 flex-row my-auto text-white pr-60">
+        <p className="text-2xl text-orange-500 font-semibold border-2 border-b-orange-500 border-x-primary border-t-primary w-fit px-2 mb-4">
+          {menu.category}
+        </p>
+        <h3 className="text-6xl font-bold">{menu.name}</h3>
+        <p className="text-4xl font-semibold pb-4">${menu.price}</p>
+        <p className="text-white opacity-60">Description</p>
+        <p className="text-justify">{menu.description}</p>
+      </div>
+    </div>
+  );
 }
